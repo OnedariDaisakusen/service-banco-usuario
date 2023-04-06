09435c0c3519642ac4ae7d25120932ed659e6eac
Blast <novagit666@gmail.com>
2023-04-06 01:22:35 -0500

    cors

--- src/main/java/com/banco/franco/servicebancousuario/controller/DniController.java
+++ src/main/java/com/banco/franco/servicebancousuario/controller/DniController.java
@@ -3,12 +3,10 @@ package com.banco.franco.servicebancousuario.controller;
 import com.banco.franco.servicebancousuario.service.DniService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/dni")
 public class DniController {
 
